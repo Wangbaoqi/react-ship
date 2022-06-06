77cd8a07cd1e2cbc1195b56db1cda6bf0bc2cbc2
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-06-06 16:49:46 +0800

    bugfix: ouput change

--- .umirc.ts
+++ .umirc.ts
@@ -6,9 +6,8 @@ export default defineConfig({
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'docs-dist',
+  outputPath: 'public',
   mode: 'site',
-
   locales: [['zh-CN', '中文'], ['en-US', 'English']],
   navs: {
     'en-US': [
